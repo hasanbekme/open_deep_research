c71e54c872fc71792ec0ff2224e340ade69f0077
mawido <mawidob@gmail.com>
2025-05-21 06:18:36 +0300

    Graph modified to use Google Gemini

--- src/open_deep_research/configuration.py
+++ src/open_deep_research/configuration.py
@@ -38,20 +38,20 @@ class Configuration:
     search_api_config: Optional[Dict[str, Any]] = None
     
     # Graph-specific configuration
-    number_of_queries: int = 2 # Number of search queries to generate per iteration
-    max_search_depth: int = 2 # Maximum number of reflection + search iterations
-    planner_provider: str = "anthropic"  # Defaults to Anthropic as provider
-    planner_model: str = "claude-3-7-sonnet-latest" # Defaults to claude-3-7-sonnet-latest
+    number_of_queries: int = 5 # Number of search queries to generate per iteration
+    max_search_depth: int = 5 # Maximum number of reflection + search iterations
+    planner_provider: str = "google_genai"  # Defaults to Anthropic as provider
+    planner_model: str = "gemini-2.0-flash" # Defaults to claude-3-7-sonnet-latest
     planner_model_kwargs: Optional[Dict[str, Any]] = None # kwargs for planner_model
-    writer_provider: str = "anthropic" # Defaults to Anthropic as provider
-    writer_model: str = "claude-3-5-sonnet-latest" # Defaults to claude-3-5-sonnet-latest
+    writer_provider: str = "google_genai" # Defaults to Anthropic as provider
+    writer_model: str = "gemini-2.0-flash" # Defaults to claude-3-5-sonnet-latest
     writer_model_kwargs: Optional[Dict[str, Any]] = None # kwargs for writer_model
     search_api: SearchAPI = SearchAPI.TAVILY # Default to TAVILY
     search_api_config: Optional[Dict[str, Any]] = None 
     
     # Multi-agent specific configuration
-    supervisor_model: str = "openai:gpt-4.1" # Model for supervisor agent in multi-agent setup
-    researcher_model: str = "openai:gpt-4.1" # Model for research agents in multi-agent setup 
+    supervisor_model: str = "google_genai:gemini-2.0-flash" # Model for supervisor agent in multi-agent setup
+    researcher_model: str = "google_genai:gemini-2.0-flash" # Model for research agents in multi-agent setup 
 
     @classmethod
     def from_runnable_config(
